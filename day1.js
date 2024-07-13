762ff3020b8388089e5e1f27a27f28c21e1570ee
Lordmayur <148192595+Lordmayur@users.noreply.github.com>
2024-07-13 13:16:01 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,50 @@
+//DAY-1 : VARIABLES AND DATATYPES
+
+//declaring variable using var , assigning it a number
+var account=121232;
+console.log(account);
+
+//declaring variable using let, assigning it a string
+let accountName="jack";
+console.log(accountName);
+
+//declaring variable using const, assigning it a boolean value
+const myBoolean=true;
+console.log(myBoolean);
+
+
+
+//creating variables of different data types and log each variable using typeof operator.
+ 
+//string
+let mystring="jack"
+console.log(typeof mystring);
+
+//number
+let mynum=7;
+console.log(typeof mynum);
+
+//boolean
+let mybool=true;
+console.log(typeof mybool);
+
+// Object
+const myObject = {
+    name: "John",
+    age: 30,
+    isStudent: true
+  };
+  console.log(typeof myobject); 
+  
+// Array
+const myArray = [1, 2, 3, 4, 5];
+console.log(typeof myarray); 
+
+//declare a variable using let, assign it an initial value, reassign a new value,and log both values to the console
+let myinitial=10;
+console.log(myinitial);
+
+myinitial=20;
+console.log(myinitial);
+
+//understanding const
